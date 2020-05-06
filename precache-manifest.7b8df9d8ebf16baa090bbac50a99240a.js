1e4578d51857aca77d394d338a713d328c9a9421
Luka Kukina <lukscbt@gmail.com>
2020-05-06 22:01:20 +0200

    Updates

---
+++ precache-manifest.7b8df9d8ebf16baa090bbac50a99240a.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c195d18f54040590b8d634847a3852a",
+    "url": "/CoffeeBlend/index.html"
+  },
+  {
+    "revision": "d0ab290cefe776e33159",
+    "url": "/CoffeeBlend/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "e47ff22582231b45ee41",
+    "url": "/CoffeeBlend/static/css/main.f553a3a1.chunk.css"
+  },
+  {
+    "revision": "d0ab290cefe776e33159",
+    "url": "/CoffeeBlend/static/js/2.3dbb0b17.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CoffeeBlend/static/js/2.3dbb0b17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47ff22582231b45ee41",
+    "url": "/CoffeeBlend/static/js/main.e0039a98.chunk.js"
+  },
+  {
+    "revision": "80d7be66f765536cd11a",
+    "url": "/CoffeeBlend/static/js/runtime-main.03174a44.js"
+  },
+  {
+    "revision": "53e7427de4aaa693950c7a9c35de80e2",
+    "url": "/CoffeeBlend/static/media/GreatVibes-Regular.53e7427d.otf"
+  },
+  {
+    "revision": "69b979e6a9c21f63e9b49771f44eeb78",
+    "url": "/CoffeeBlend/static/media/about.69b979e6.jpg"
+  },
+  {
+    "revision": "bd212343ee4b1e05ba04e0218db2a2fb",
+    "url": "/CoffeeBlend/static/media/bg_1.bd212343.jpg"
+  },
+  {
+    "revision": "ef2ee04a896b6984e3690763c0881254",
+    "url": "/CoffeeBlend/static/media/bg_2.ef2ee04a.jpg"
+  },
+  {
+    "revision": "e292fa66ba0bd3004687ae251b4fed2c",
+    "url": "/CoffeeBlend/static/media/bg_3.e292fa66.jpg"
+  },
+  {
+    "revision": "74acd028bb854b22bae8b1b83fc8a477",
+    "url": "/CoffeeBlend/static/media/bg_4.74acd028.jpg"
+  },
+  {
+    "revision": "0084708bc326b6b25dd4f3d79bd9e665",
+    "url": "/CoffeeBlend/static/media/coffee-beans.0084708b.svg"
+  },
+  {
+    "revision": "1f2c7b0521879b3a7e7b5a4edac2e6c5",
+    "url": "/CoffeeBlend/static/media/coffee-cup.1f2c7b05.svg"
+  },
+  {
+    "revision": "350b200bc75072261be1b6ba6098520d",
+    "url": "/CoffeeBlend/static/media/delivery-truck.350b200b.svg"
+  },
+  {
+    "revision": "af3e9df4fef626855bc17cef000776c6",
+    "url": "/CoffeeBlend/static/media/dessert-1.af3e9df4.jpg"
+  },
+  {
+    "revision": "95fce57dcf2c5a885ac8d15574a67145",
+    "url": "/CoffeeBlend/static/media/dessert-2.95fce57d.jpg"
+  },
+  {
+    "revision": "12087a9b720bcf29b73f6a516a62d099",
+    "url": "/CoffeeBlend/static/media/dessert-3.12087a9b.jpg"
+  },
+  {
+    "revision": "4cc09b958b27efe79adeb4c6f60f73cb",
+    "url": "/CoffeeBlend/static/media/dessert-4.4cc09b95.jpg"
+  },
+  {
+    "revision": "221592ddee5959e81b7f209a6cf1dc64",
+    "url": "/CoffeeBlend/static/media/dessert-5.221592dd.jpg"
+  },
+  {
+    "revision": "aade9e1fce9dff6360612b59e985cea9",
+    "url": "/CoffeeBlend/static/media/dessert-6.aade9e1f.jpg"
+  },
+  {
+    "revision": "0f1da2e92b43ecda84c449b5739781b8",
+    "url": "/CoffeeBlend/static/media/drink-1.0f1da2e9.jpg"
+  },
+  {
+    "revision": "0233b2628d6d46251274b774e5634899",
+    "url": "/CoffeeBlend/static/media/drink-2.0233b262.jpg"
+  },
+  {
+    "revision": "1a4f5bb2a6c6e064164e3cd273b8be2f",
+    "url": "/CoffeeBlend/static/media/drink-3.1a4f5bb2.jpg"
+  },
+  {
+    "revision": "007dd55d6287956394b2d4defbc0f53a",
+    "url": "/CoffeeBlend/static/media/drink-4.007dd55d.jpg"
+  },
+  {
+    "revision": "3f12646999f37233fd850e77aad3faa5",
+    "url": "/CoffeeBlend/static/media/drink-5.3f126469.jpg"
+  },
+  {
+    "revision": "4d6b66bb8dd4fa511c9f64fc976fa982",
+    "url": "/CoffeeBlend/static/media/drink-6.4d6b66bb.jpg"
+  },
+  {
+    "revision": "5b5ea837b6e57b4c872f705dcc308a7d",
+    "url": "/CoffeeBlend/static/media/gallery-1.5b5ea837.jpg"
+  },
+  {
+    "revision": "13f35482d02ab355e33e30cbb154a6ef",
+    "url": "/CoffeeBlend/static/media/gallery-2.13f35482.jpg"
+  },
+  {
+    "revision": "d558fc91ea03ed609a7aba6e2a22e442",
+    "url": "/CoffeeBlend/static/media/gallery-3.d558fc91.jpg"
+  },
+  {
+    "revision": "abb3c8323dfe76bb36ab1c151c9bf057",
+    "url": "/CoffeeBlend/static/media/gallery-4.abb3c832.jpg"
+  },
+  {
+    "revision": "d9d208a8fdf6b1d9faf33baae414944f",
+    "url": "/CoffeeBlend/static/media/logo1.d9d208a8.svg"
+  },
+  {
+    "revision": "ffedbd034e3f133f7c119af656a8cdee",
+    "url": "/CoffeeBlend/static/media/logo2.ffedbd03.svg"
+  },
+  {
+    "revision": "b120f24262b3e7db4c2ef6f98235450f",
+    "url": "/CoffeeBlend/static/media/logo3.b120f242.svg"
+  },
+  {
+    "revision": "cb79f23244b58bc44ce9dff9305c4dc9",
+    "url": "/CoffeeBlend/static/media/logo3.cb79f232.svg"
+  },
+  {
+    "revision": "0e08ce6f45636dbdca1585761df2df51",
+    "url": "/CoffeeBlend/static/media/logotype.0e08ce6f.svg"
+  },
+  {
+    "revision": "ea056d93940037b0d814a82d8cdb098f",
+    "url": "/CoffeeBlend/static/media/logotype2.ea056d93.svg"
+  },
+  {
+    "revision": "87fdbe4779a09d178853992a03ccbdcc",
+    "url": "/CoffeeBlend/static/media/logotype3.87fdbe47.svg"
+  },
+  {
+    "revision": "a17494fa8b4ba5cdda042f02f1117002",
+    "url": "/CoffeeBlend/static/media/mainDish-1.a17494fa.jpg"
+  },
+  {
+    "revision": "c6c92b523a964194d5f8938c11716b4a",
+    "url": "/CoffeeBlend/static/media/mainDish-2.c6c92b52.jpg"
+  },
+  {
+    "revision": "db2186630586b49668d6e41e8f974007",
+    "url": "/CoffeeBlend/static/media/mainDish-3.db218663.jpg"
+  },
+  {
+    "revision": "69d97f623c7af5e3f68ef1ae4cbbc9ba",
+    "url": "/CoffeeBlend/static/media/mainDish-4.69d97f62.jpg"
+  },
+  {
+    "revision": "2ef259decdf96021fbddb92e52bcf890",
+    "url": "/CoffeeBlend/static/media/mainDish-5.2ef259de.jpg"
+  },
+  {
+    "revision": "ab49fc4a64522a3d258290ba498191e8",
+    "url": "/CoffeeBlend/static/media/mainDish-6.ab49fc4a.jpg"
+  },
+  {
+    "revision": "18c1aa2a08980c6a2d5533f3622e16ee",
+    "url": "/CoffeeBlend/static/media/menu-1.18c1aa2a.jpg"
+  },
+  {
+    "revision": "8b4ad3e8a87200dc31035c632d826345",
+    "url": "/CoffeeBlend/static/media/menu-2.8b4ad3e8.jpg"
+  },
+  {
+    "revision": "80280f1718cb8762364e644177b56f1c",
+    "url": "/CoffeeBlend/static/media/menu-3.80280f17.jpg"
+  },
+  {
+    "revision": "1643a4d74a732475202568beca2d2e98",
+    "url": "/CoffeeBlend/static/media/menu-4.1643a4d7.jpg"
+  },
+  {
+    "revision": "c949a21bcb635db826fcdf8d6441459a",
+    "url": "/CoffeeBlend/static/media/order.c949a21b.svg"
+  },
+  {
+    "revision": "7422961bec8be78e896ad981483aee87",
+    "url": "/CoffeeBlend/static/media/person_2.7422961b.jpg"
+  },
+  {
+    "revision": "f74eb6631188b8420d222a10d71d5502",
+    "url": "/CoffeeBlend/static/media/person_3.f74eb663.jpg"
+  },
+  {
+    "revision": "099be3dd5561d4e167f343c401e8d4af",
+    "url": "/CoffeeBlend/static/media/person_4.099be3dd.jpg"
+  },
+  {
+    "revision": "1c206464a1f59abe821abfe73fdc3303",
+    "url": "/CoffeeBlend/static/media/search3.1c206464.svg"
+  }
+]);
